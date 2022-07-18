f418fde492f7b016e1f696cd06405898d3cfb688
Lolloc99 <lolloc99@yahoo.it>
2022-07-18 11:00:36 +0200

    Create tabelle e stabilite le relazioni tra di esse

---
+++ database/migrations/2022_07_18_084922_add_nationality_id_to_athletes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNationalityIdToAthletesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('athletes', function (Blueprint $table) {
+            $table->unsignedBigInteger('nationality_id');
+            $table->foreign('nationality_id')
+                ->references('id')
+                ->on('nationalities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('athletes', function (Blueprint $table) {
+            //
+        });
+    }
+}
